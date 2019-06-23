0417f16f8a9168c117f5dc6907cae2e2ed482ade
Ivan Miljancic <ivan.miljancic@gmail.com>
2019-06-23 23:58:42 +0200

    Created "Post" form and configured database

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostFormType;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -14,27 +21,43 @@ class DefaultController extends AbstractController {
 
 	/**
 	 * @Route("/", name="index")
+	 * @param Request $request
+	 *
+	 * @return RedirectResponse|Response
 	 */
-	public function index() {
-		$posts = [
-			[
-				'title' => 'Title 2',
-				'body' => 'Body 1',
-				'postDate' => date('d-m-y H:i:s')
-			],
-			[
-				'title' => 'Title 2',
-				'body' => 'Body 2',
-				'postDate' => date('d-m-y H:i:s')
-			],
-			[
-				'title' => 'Title 3',
-				'body' => 'Body 3',
-				'postDate' => date('d-m-y H:i:s')
-			],
-		];
-		return $this->render( 'default/index.html.twig', [
-			'posts' => $posts
+	public function index( Request $request ) {
+		$post = new Post();
+
+		$form = $this->createForm( PostFormType::class, $post );
+
+		$form->add('submit', SubmitType::class, [
+			'label' => 'Create post',
+			'attr' => [
+				'class' => 'btn btn-primary'
+			]
 		]);
+
+		$form->handleRequest( $request );
+
+		if ( $form->isSubmitted() && $form->isValid() ) {
+			$post = $form->getData();
+			$post->setPostDate(DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s')));
+
+			$em = $this->getDoctrine()->getManager();
+
+			$em->persist( $post );
+			$em->flush();
+
+			$this->addFlash('success', 'Post is successfully created!');
+
+			return $this->redirectToRoute('default.index');
+		}
+
+		$posts = $this->getDoctrine()->getRepository( Post::class )->findAll();
+
+		return $this->render( 'default/index.html.twig', [
+			'posts' => $posts,
+			'form' => $form->createView()
+		] );
 	}
 }
---
+++ src/Entity/Post.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $body;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $post_date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getPostDate(): ?\DateTimeInterface
+    {
+        return $this->post_date;
+    }
+
+    public function setPostDate(?\DateTimeInterface $post_date): self
+    {
+        $this->post_date = $post_date;
+
+        return $this;
+    }
+}
